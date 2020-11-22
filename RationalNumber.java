9b59be9ab5db033c6867cdf9ce3f5141a9f8b073
shane-decatur <sdecatur20@stuy.edu>
2020-11-21 22:17:12 -0500

    gcd() added, hopefully works fine

--- RationalNumber.java
+++ RationalNumber.java
@@ -12,6 +12,7 @@ public class RationalNumber extends RealNumber
       numerator = nume;
       denominator = deno;
     }
+    reduce();
   }
 
   public double getValue(){
@@ -40,5 +41,37 @@ public class RationalNumber extends RealNumber
 
   public String toString(){
     return numerator+"/"+denominator;
+  }
+
+  public static int gcd(int a, int b){
+    if (a == 0 || b == 0){
+      return 1;
+    }
+    int divisor = 1;
+    if (a > b){
+      int r = a % b;
+      int dividend = b;
+      while (r != 0){
+        divisor = r;
+        r = dividend % divisor;
+        dividend = divisor;
+      }
+    }
+    else if (b > a){
+      int r = b % a;
+      int dividend = a;
+      while (r != 0){
+        divisor = r;
+        r = dividend % divisor;
+        dividend = divisor;
+      }
+    }
+    return divisor;
+  }
+
+  private void reduce(){
+    int gcd = gcd(numerator, denominator);
+    numerator = numerator / gcd;
+    denominator = denominator / gcd;
   }
 }
