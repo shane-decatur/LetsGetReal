3e9a1dae01b927f40a3acc9103c4902d224d67b8
shane-decatur <sdecatur20@stuy.edu>
2020-11-21 21:15:23 -0500

    added RationalNumber and RealNumber files

---
+++ RationalNumber.java
@@ -0,0 +1,10 @@
+public class RationalNumber extends RealNumber
+{
+  private int numerator, denominator;
+
+  public RationalNumber(int nume, int deno){
+    super(0.0);//this value is ignored!
+  }
+
+
+}
