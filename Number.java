447a41cd6dc8f979d49d1dbffb4f056072c96e5f
shane-decatur <sdecatur20@stuy.edu>
2020-11-24 17:04:51 -0500

    added compareTo()

--- Number.java
+++ Number.java
@@ -1,7 +1,17 @@
 public abstract class Number{
   public abstract double getValue();
 
-  
+  public int compareTo(Number other){
+    if (equals(other)){
+      return 0;
+    }
+    if (getValue() > other.getValue()){
+      return 1;
+    }
+    else{
+      return -1;
+    }
+  }
 
   public boolean equals(Number other){
     if (getValue() == 0 || other.getValue() == 0){
