42490598af38e44bc9a924aba7baffd6c2ad1dfe
shane-decatur <sdecatur20@stuy.edu>
2020-11-24 17:02:34 -0500

    added equals method to class

--- Number.java
+++ Number.java
@@ -1,3 +1,15 @@
 public abstract class Number{
   public abstract double getValue();
+
+  
+
+  public boolean equals(Number other){
+    if (getValue() == 0 || other.getValue() == 0){
+      return (getValue() == 0 && other.getValue() == 0);
+    }
+    else if (Math.abs(other.getValue() - getValue()) / getValue() <= 0.00001){
+      return true;
+    }
+    return false;
+  }
 }
